3424348d8e5f104ca76c9aef318d6cc9e3b0b072
danielFernandezJd <mdanif9728@gmail.com>
2024-05-06 19:21:22 -0700

    sending index and show data

--- views/index.jsx
+++ views/index.jsx
@@ -2,9 +2,8 @@ const React = require("react");
 const Default = require("./layouts/default");
 
 function Index({ breads }) {
-  console.log("breads", breads);
   return (
-    <Default>
+    <Default title={title}>
       <h2>Index Page</h2>
       {/* <p>I have {breads[0].name} bread!</p> */}
       {/* This is a JSX comment. */}
