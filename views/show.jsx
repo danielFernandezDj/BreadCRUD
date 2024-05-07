b71b8efd4949d93ecf29f6a373b66c33919dd2a7
danielFernandezJd <mdanif9728@gmail.com>
2024-05-06 20:24:45 -0700

    add some code to the sshow.jsx

--- views/show.jsx
+++ views/show.jsx
@@ -1,15 +1,16 @@
-const React = require('react')
-const Default = require('./layouts/default')
+const React = require("react");
+const Default = require("./layouts/default");
 
-function Show ({bread}) {
-    console.log(bread.name)
-    return (
-        <Default>
-            <h2>
-                Show Page
-            </h2>
-        </Default>
-    )
+function Show({ bread }) {
+  // Confirm we are getting our bread data in the terminal.
+  // console.log(bread.name)
+  return (
+    <Default>
+      <h2> Show Page </h2>
+      <h3>{bread.name}</h3>
+      <img src={bread.image} alt={bread.image} />
+    </Default>
+  );
 }
 
-module.exports = Show
+module.exports = Show;
