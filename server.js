bbaecf871db9f003750b173ea65878c0acc99a5a
danielFernandezJd <mdanif9728@gmail.com>
2024-05-08 20:25:54 -0700

    create and new routes complete

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
 // ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
@@ -20,14 +27,11 @@ app.listen(PORT, () => {
     console.log('listening on port', PORT);
 })
 
-// MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
 
 // 404 Page
 app.get('*', (req, res) => {
     res.send(`<h1>404</h1>`)
 })
+
+
 
