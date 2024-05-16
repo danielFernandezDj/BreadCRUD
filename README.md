41ac476286c2ff44bcd3fc9b770fa25b56e00f6d
danielFernandezJd <mdanif9728@gmail.com>
2024-05-15 21:38:32 -0700

    Add a <p> to show.jsx

---
+++ README.md
@@ -0,0 +1,17 @@
+# Breads
+
+clone repo
+run 'npm install'
+
+## to start server
+run 'nodemon'
+
+## to see all breads
+http:/localhost:3003/breads
+
+## to see a bread
+<<<<<<< HEAD
+http:/localhost:3003/breads/1
+=======
+http:/localhost:3003/breads/1
+>>>>>>> 51c748f (create a folder views and add a jsx file)
---
+++ views/index.jsx
@@ -0,0 +1,29 @@
+const React = require("react");
+const Default = require("./layouts/default");
+
+function Index({ breads, title }) {
+  return (
+    <Default title={title}>
+      <h2>Index Page</h2>
+      {/* <p>I have {breads[0].name} bread!</p> */}
+      {/* This is a JSX comment. */}
+      <div className="newButton">
+        <a href="/breads/new">
+          <button>Add a new bread</button>
+        </a>
+      </div>
+
+      <ul>
+        {breads.map((bread, index) => {
+          return (
+            <li key={index}>
+              <a href={`/breads/${bread.id}`}>{bread.name}</a> 
+            </li>
+          );
+        })}
+      </ul>
+    </Default>
+  );
+}
+
+module.exports = Index;
