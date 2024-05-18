0bf15beaa8b30df658d8aacb054d9c49797cdc86
danielFernandezJd <mdanif9728@gmail.com>
2024-05-18 00:20:23 -0700

    fix the default path at new.jsx

--- views/new.jsx
+++ views/new.jsx
@@ -1,7 +1,7 @@
-const React = require('react')
-const Default = require('./layouts/default')
+const React = require('react');
+const Default = require('./layouts/default');
 
-function New() {
+function New({ bakers }) {
   return (
     <Default>
       <h2>Add a new bread</h2>
@@ -26,15 +26,12 @@ function New() {
           id="image"
         />
 
-        {/* Baler */}
+        {/* Baker */}
         <label htmlFor="baker">Baker</label>
         <select name="baker" id="baker">
-          <option value="Rachel">Rachel</option>
-          <option value="Monica">Monica</option>
-          <option value="Joey">Joey</option>
-          <option value="Chandler">Chandler</option>
-          <option value="Ross">Ross</option>
-          <option value="Phoebe">Phoebe</option>
+          {bakers.map((baker) => (
+            <option value={baker.id} key={baker.id}>{baker.name}</option>
+          ))}
         </select>
 
         <label htmlFor="hasGluten">Has Gluten?</label>
@@ -48,10 +45,9 @@ function New() {
 
         <br />
         <input type="submit" />
-
       </form>
     </Default>
   )
 }
 
-module.exports = New
+module.exports = New;
