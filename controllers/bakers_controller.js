2f466a817b20a139b69c1a0f659f319c80587422
danielFernandezJd <mdanif9728@gmail.com>
2024-05-29 22:42:07 -0700

    try to fix vercel

---
+++ controllers/bakers_controller.js
@@ -0,0 +1,54 @@
+// dependencies
+const express = require('express')
+const baker = express.Router()
+const Baker = require('../models/baker.js')
+const bakerSeedData = require('../models/baker_seed.js')
+
+// INDEX
+baker.get('/', (req, res) => {
+    Baker.find()
+        .populate('breads')
+        .then(foundBakers => {
+            res.send(foundBakers)
+        })
+})
+
+// SEEDS
+baker.get('/data/seed', (req, res) => {
+    Baker.insertMany(bakerSeedData)
+        .then(() => {
+            res.redirect('/breads')
+        })
+        .catch((err) => {
+            res.status(400).json({
+                message: 'Seed unsuccessful',
+                error: err.message
+            })
+        })
+})
+
+// Show: 
+baker.get('/:id', (req, res) => {
+    Baker.findById(req.params.id)
+        .populate({
+            path: 'breads',
+            options: { limit: 5 }
+        })
+        .then(foundBaker => {
+            res.render('bakerShow', {
+                baker: foundBaker
+            })
+        })
+})
+
+// delete
+baker.delete('/:id', (req, res) => {
+    Baker.findByIdAndDelete(req.params.id)
+        .then(deletedBaker => {
+            res.status(303).redirect('/breads')
+        })
+})
+
+
+// export
+module.exports = baker                    
---
+++ views/new.jsx
@@ -0,0 +1,53 @@
+const React = require('react');
+const Default = require('./layouts/default');
+
+function New({ bakers }) {
+  return (
+    <Default>
+      <h2>Add a new bread</h2>
+      <div className="backButton">
+        <a href="/breads"><button>Go back to the index</button></a>
+      </div>
+
+      <form action='/breads' method='POST'>
+        <label htmlFor="name">Name</label>
+
+        <input
+          type="text"
+          name="name"
+          id="name"
+          required
+        />
+
+        <label htmlFor="image">Image</label>
+        <input
+          type="text"
+          name="image"
+          id="image"
+        />
+
+        {/* Baker */}
+        <label htmlFor="baker">Baker</label>
+        <select name="baker" id="baker">
+          {bakers.map((baker) => (
+            <option value={baker.id} key={baker.id}>{baker.name}</option>
+          ))}
+        </select>
+
+        <label htmlFor="hasGluten">Has Gluten?</label>
+
+        <input
+          type="checkbox"
+          name="hasGluten"
+          id="hasGluten"
+          defaultChecked
+        />
+
+        <br />
+        <input type="submit" />
+      </form>
+    </Default>
+  )
+}
+
+module.exports = New;
