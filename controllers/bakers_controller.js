09686939a373693bc117d81d6f88cf7cdfd62130
danielFernandezJd <mdanif9728@gmail.com>
2024-05-29 20:52:36 -0700

    create a vercel.json

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -1,7 +1,7 @@
 // dependencies
 const express = require('express')
 const baker = express.Router()
-const Baker = require('../models/baker.js')
+const BakerData = require('../models/baker.js')
 const bakerSeedData = require('../models/baker_seed.js')
 
 // INDEX
